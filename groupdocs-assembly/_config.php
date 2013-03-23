bd4daa1c55e5c3b0a34544fb145ce863aab5ba79
averjr <averkiev.jr@gmail.com>
2013-03-23 23:40:00 +0200

    version 1.0.0

---
+++ groupdocs-assembly/_config.php
@@ -0,0 +1,4 @@
+<?php
+
+
+ShortcodeParser::get('default')->register('groupdocsAssembly', array('groupdocsAssembly', 'handle_shortcode'));
---
+++ groupdocs-assembly/code/Result.php
@@ -0,0 +1,20 @@
+<?php
+
+class groupdocsAssembly_Result extends ViewableData {
+	protected $width = false;
+	protected $height = false;
+	protected $guid = false;
+
+	
+	public function __construct($width, $height, $guid) {
+		$this->width = $width;
+		$this->height = $height;
+		$this->guid = $guid;
+		parent::__construct();
+	}
+	
+	public function forTemplate() {
+		return "<iframe src='https://apps.groupdocs.com/assembly2/questionnaire-assembly/" . $this->guid . "?&referer=silverstripe/1.0.0' frameborder='0' width='" . $this->width . "' height='" . $this->height ."'></iframe>";
+	}
+	
+}
